dfcff59ca496bd2c28b951afa11aef7b738d8e70
Charis Philip Wibowo <charisphilipwibowo@gmail.com>
2026-06-13 16:37:24 +0700

    update baseurl 1

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: 'https://edu-adapt-gt2pq93c7-charisphilipwibowo-uis-projects.vercel.app' });
+const API = axios.create({ baseURL: 'https://edu-adapt-9m49fg4or-charisphilipwibowo-uis-projects.vercel.app' });
 
 API.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
